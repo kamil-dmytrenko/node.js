ac77a946f8f41c4b73b45b167bd3a4cfb00f6dcf
Unknown <kamil.dmytrenko@gmail.com>
2018-03-27 03:54:53 +0200

    update routes

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // This is the main file of our app. It initializes a new
-// express.js instance, requires the config and routes files
+// express.js instance, requires the config file
 // and listens on a port. Start the application by running
 // 'node app.js' in your terminal
 
@@ -8,7 +8,7 @@ const express  = require('express'),
 	  app      = express();
 
 const Film    = require('./models/film'),
-    Comment = require('./models/comment');
+      Comment = require('./models/comment');
 
 // Require the configuration and pass
 // the app as argument to the returned functions.
@@ -19,107 +19,92 @@ app.get('/', (req, res) => {
    res.render('index');
 });
 
-// list of all movies already present in application database
+//list of all movies in db
 app.get('/movies', (req, res) => {
     Film.find({}, (err, films) => {
-        if (err) {
-            res.send(err);
-        } else {
-            res.send(films);
-        }
+        if (err) throw err;
+        res.render('films/show-all', {films:films});
     });
 });
 
 // add new film to db if present in OMDB by passing film title in request body
 app.post('/movies', (req, res) => {
-    let url = 'http://www.omdbapi.com/?apikey=thewdb&t='+req.body.title;
-    request(url, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-            const result = JSON.parse(body);
-            if(result.Title) {
-                db.collection('films').insert(result, function (err, result) {
-                    if (err) {
-                        res.send(err);
-                    }
-                    else {
-                        res.render('films/show-one', {film: result});
-                    }
-                });
-            } else if (!result.Title){
-                res.send(result.Error);
-            }
-        }
+    db.collection('films').findOne({"Title": req.body.title}, (err, foundFilm) =>{
+       if (err) throw err;
+       if (foundFilm) {
+           res.render('films/show-new', {film:foundFilm});
+           // res.send(foundFilm);
+       } else {
+           let url = 'http://www.omdbapi.com/?apikey=thewdb&t='+req.body.title;
+           request(url, (error, response, body) => {
+               if (!error && response.statusCode === 200) {
+                   const result = JSON.parse(body);
+                   if(result.Title) {
+                       const newFilm = new Film({
+                           Title: req.body.title,
+                       });
+                       Film.create(newFilm, (err, newFilm) => {
+                           db.collection('films').update(newFilm, result, {upsert: true}, (err, updated) => {
+                               if (err) throw err;
+                               Film.findOne({"Title": result.Title}, (err, foundFilm) => {
+                                   if (err) throw err;
+                                   res.render('films/show-new', {film:foundFilm});
+                                   // res.send(foundFilm);
+                               })
+                           });
+                       });
+                   } else if (!result.Title){
+                       res.send(result.Error);
+                   }
+               }
+           });
+       }
     });
 });
 
 // find all comments present in db
 app.get('/comments', (req, res) => {
-    //find associated with specified film NIE DZIAŁA 
-    // Film.findById(req.params.filmId).populate("comments").exec((err, foundFilm) => {
-    //     if (err) {
-    //         res.send(err)
-    //     } else {
-    //         // res.render('comments/show-by-film', {foundFilm: foundFilm})
-    //         res.send(foundFilm)
-    //     }
-    // });
-
-    //find all DZIAŁA
     Comment.find({}, (err, comments) => {
-        if (err) {
-            res.send(err);
-        } else {
-            res.render('comments/show-all', {comments: comments});
-        }
+        if (err) throw err;
+        res.render('comments/show-all', {comments:comments});
     });
 });
 
 //add new comment to film by passing film id and comment text in request body
 app.post('/comments', (req, res) => {
-   Film.findById(req.body.filmId, (err, foundFilm) => {
+    const filmID = req.body.film._id;
+    const comment_text = req.body.comment.text;
+    Film.findById(filmID, (err, foundFilm) => {
        if (err) {
            res.send(err);
        } else {
-           console.log(foundFilm);
-           Comment.create(req.body, (err, comment) => {
-               if (err) {
-                   res.send(err);
-               } else {
-                   comment.text = req.body.text;
-                   comment.save();
-                   console.log(comment);
-                   console.log(foundFilm);
-                   // foundFilm.comments.push(comment);
-                   // foundFilm.save();
-                   // res.redirect('/movies');
-               }
-           })
+           const newComment = new Comment({
+              text:comment_text
+           });
+           Comment.create(newComment, (err, comment) => {
+               comment.save();
+               foundFilm.comments.push(comment);
+               foundFilm.save();
+               Comment.findById(comment._id, (err, foundComment) => {
+                   if (err) throw err;
+                   res.send(foundComment.text);
+               });
+           });
        }
-   })
+    })
 });
 
-function seed_db_with_comments() {
-    Film.find({}, (err, films) => {
-        if (err) {
-            console.log("Error" + err);
-        } else {
-            films.forEach((film) => {
-                Comment.create({
-                    text: film.Title + ' is great'
-                }, (err, comment) => {
-                    if(err){
-                        console.log(err);
-                    } else {
-                        film.comments.push(comment);
-                        film.save();
-                        console.log("Created new comment");
-                    }
-                })
-            });
-        }
+function findAllCommentsWithAssociatedFilmName() {
+    Film.find({}, (err, foundFilms) => {
+        if (err) throw err;
+        Comment.find({}, (err, foundComments) => {
+            if (err) throw err;
+
+        })
     });
 }
-// seed_db_with_comments();
 
+// findAllCommentsWithAssociatedFilmName();
 
+module.exports = app;
 
