f91ccc3a1b0d8ad3f86adfb593a30c81e18bdd4a
Unknown <kamil.dmytrenko@gmail.com>
2018-03-27 03:50:35 +0200

    update film model

--- models/film.js
+++ models/film.js
@@ -3,6 +3,43 @@ const mongoose = require('mongoose');
 
 module.exports = mongoose.model('Film', new mongoose.Schema({
     Title: { type : String , unique : true, required : true, dropDups: true },
+    Year: String,
+    Rated: String,
+    Released: String,
+    Runtime: String,
+    Genre: String,
+    Director: String,
+    Writer: String,
+    Actors: String,
+    Plot: String,
+    Language: String,
+    Country: String,
+    Awards: String,
+    Poster: String,
+    Ratings : [
+        {
+            Source : String,
+            Value : String
+        },
+        {
+            Source : String,
+            Value : String
+        },
+        {
+            Source : String,
+            Value : String
+        }
+    ],
+    Metascore : String,
+    imdbRating : String,
+    imdbVotes : String,
+    imdbID : String,
+    Type : String,
+    DVD : String,
+    BoxOffice : String,
+    Production : String,
+    Website : String,
+    Response : String,
     comments: [
         {
             type: mongoose.Schema.Types.ObjectId,
@@ -10,5 +47,4 @@ module.exports = mongoose.model('Film', new mongoose.Schema({
         }
     ]
 }));
-
 
