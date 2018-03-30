0e21c7be7c2333ca75d1fdf728710671bc29cb9e
Unknown <kamil.dmytrenko@gmail.com>
2018-03-30 04:52:58 +0200

    add script for filtering comments

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,4 @@
-$(document).ready(function()
-    {
-        $("#myTable").tablesorter( {sortList: [[1,0],[2,0],[3,0],[4,0]]} );
-    }
-);
+
+let commentsList = new List('comments', {
+    valueNames: ['filmTitle']
+});
