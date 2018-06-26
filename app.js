e0c9d733c066b74300e1edbea0eb986e79f01870
Kamil <kamil.dmytrenko@gmail.com>
2018-06-26 15:03:08 +0200

    refactor main app file

--- app.js
+++ app.js
@@ -1,27 +1,16 @@
-// This is the main file of our app. It initializes a new
-// express.js instance, requires the config file
-// and listens on a port. Start the application by running
-// 'node app.js' in your terminal
+const express = require('express');
+const app = express();
 
-const express  = require('express'),
-	  app      = express();
-
-const Film    = require('./models/film'),
-      Comment = require('./models/comment');
-
-// Require the configuration and pass
-// the app as argument to the returned functions.
-require('./config')(app);
-require('./controllers/errorController');
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+require('./db')(app);
 
 //requiring routes
-const commentRoutes    = require("./routes/comments"),
-      filmRoutes       = require("./routes/films"),
-      indexRoutes      = require("./routes/index");
+const commentRoutes = require("./routes/commentRoutes");
+const filmRoutes    = require("./routes/filmRoutes");
 
-app.use("/", indexRoutes);
-app.use("/movies", filmRoutes);
-app.use("/comments", commentRoutes);
+app.use("/film", filmRoutes);
+app.use("/comment", commentRoutes);
 
 module.exports = app;
 
