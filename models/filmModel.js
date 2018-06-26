345ccfcb8d896d2c2558ca431c07d456ffa9bbaa
Kamil <kamil.dmytrenko@gmail.com>
2018-06-26 15:03:59 +0200

    refactor models

--- models/filmModel.js
+++ models/filmModel.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-
 module.exports = mongoose.model('Film', new mongoose.Schema({
     Title: { type : String , unique : true, required : true, dropDups: true },
     Year: String,
